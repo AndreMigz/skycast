b78ec9a366ea48cf14b75dd30b7536bf168422f1
AndreMigz <migzarre@gmail.com>
2024-05-03 14:51:07 +0800

    Added Delete Weather Log Test

--- test/controllers/weathers_controller_test.rb
+++ test/controllers/weathers_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class WeathersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+   test '#index should be able to access the root_path' do
+    get weathers_path
+    assert_response :success
+  end
+
+  test '#destroy should be able to delete weather logs' do
+    weather = weathers(:one)
+    assert_difference('Weather.count', -1) do
+      delete weather_path(weather), params: { id: weather.id}
+    end
+  end
 end
