a02bb787f94ef90085c13dcf5a5f252afc5c2f75
AndreMigz <migzarre@gmail.com>
2024-05-03 14:48:30 +0800

    Fix typo error

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -8,7 +8,7 @@ class WeathersController < ApplicationController
     weather = Weather.find_by(id: params[:id])
     if weather.destroy
       respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.remove('weather#{weather.id}')}
+        format.turbo_stream { render turbo_stream: turbo_stream.remove("weather#{weather.id}")}
       end
     end
   end
